4edf8e3d123234c05fce256355db3b34ac35442f
ItsAnshumanPattanayak <anshumanpattanayak931@gmail.com>
2026-04-05 00:19:47 +0530

    Add Basic Problem 05

---
+++ BasicProblem05.py
@@ -0,0 +1,11 @@
+#Check if a number is positive, negative, or zero
+a = int(input("Enter the 1st number :"))
+
+if (a>0):
+    print("It's a positive number")
+elif(a<0) :
+    print("It's a negative number")
+elif(a==0) :
+    print("It's a zero")
+else :
+    print("Invalid output")
