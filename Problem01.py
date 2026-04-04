1f517d1737901a78b93a8ef333e9fa95680eb4db
ItsAnshumanPattanayak <anshumanpattanayak931@gmail.com>
2026-04-04 23:57:47 +0530

    solved probelm 1

--- Problem01.py
+++ Problem01.py
@@ -1,1 +1,6 @@
-print("hello World")
+#Take a number from user and check if it is even or odd
+user = int(input("Enter your number :"))
+if user % 2 == 0:
+    print("It's an Even number")
+else:
+    print ("It's an odd number")
