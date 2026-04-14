63bcfe59b6e66fee25b4c403ef414005d250f965
ItsAnshumanPattanayak <anshumanpattanayak931@gmail.com>
2026-04-14 18:31:52 +0530

    Add Question to Basic dictionary problem 02

--- Basic_Dictionary_02.py
+++ Basic_Dictionary_02.py
@@ -1,4 +1,10 @@
-#nested dictionary problem 
+'''
+Write an interactive Python script that continuously loops, asking the user to either 'insert' a record or 'quit'.
+If the user types 'insert', sequentially prompt them for a unique ID, a property key, and a value.
+Store this data safely in a nested dictionary where the unique ID maps to the inner key-value pair.
+Ensure the program appends new records properly without overwriting the entire existing database.
+Print the updated dictionary after each insertion, and display "Invalid Input" for any unrecognized commands.
+'''
 my_dict = {}
 while True:
     user = input("\nWhat you want to do : Insert or Quit: ")
